faf0c2c9014dabe5d7c32023fd4d76471545d196
Seriouswrong <tchamberlain@gmail.com>
2025-12-28 04:17:55 +0300

    Update google-socks5.js

--- google-socks5.js
+++ google-socks5.js
@@ -1,11 +1,18 @@
 function FindProxyForURL(url, host) {
-  // Google domains via SOCKS5
-  if (dnsDomainIs(host, ".google.com") ||
+  // Google bare domains + subdomains via SOCKS5
+  if (host == "google.com" ||
+      dnsDomainIs(host, ".google.com") ||
+      host == "googleusercontent.com" ||
       dnsDomainIs(host, ".googleusercontent.com") ||
+      host == "googleapis.com" ||
       dnsDomainIs(host, ".googleapis.com") ||
+      host == "gstatic.com" ||
       dnsDomainIs(host, ".gstatic.com") ||
+      host == "youtube.com" ||
       dnsDomainIs(host, ".youtube.com") ||
+      host == "1e100.net" ||
       dnsDomainIs(host, ".1e100.net") ||
+      host == "gvt1.com" ||
       dnsDomainIs(host, ".gvt1.com")) {
     return "SOCKS5 127.0.0.1:10808";
   }
@@ -19,6 +26,3 @@ function FindProxyForURL(url, host) {
   // Everything else direct
   return "DIRECT";
 }
-
-
-
